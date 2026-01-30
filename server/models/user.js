2d4a0818a5d4bb70c103dbab8b5e4f9328f046a9
AnubhavGitHub07 <prince1dwivedi00@gmail.com>
2026-01-31 01:30:55 +0530

    Updates

---
+++ server/models/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlength: 128,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User" , userSchema); // create user model
+
+export default User; // export user model
